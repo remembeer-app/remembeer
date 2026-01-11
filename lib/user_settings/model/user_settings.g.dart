862246fe82108bef8b44b8d2a1005f8195adc11e
Ondřej Hložek <hlozek.o1@gmail.com>
2026-01-11 02:16:07 +0100

    feat: use freezed on user_settings

--- lib/user_settings/model/user_settings.g.dart
+++ lib/user_settings/model/user_settings.g.dart
@@ -6,26 +6,31 @@ part of 'user_settings.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-UserSettings _$UserSettingsFromJson(Map<String, dynamic> json) => UserSettings(
-  id: json['id'] as String,
-  defaultDrinkType: DrinkType.fromJson(
-    json['defaultDrinkType'] as Map<String, dynamic>,
-  ),
-  defaultDrinkSize: (json['defaultDrinkSize'] as num).toInt(),
-  drinkListSort:
-      $enumDecodeNullable(_$DrinkListSortEnumMap, json['drinkListSort']) ??
-      DrinkListSort.descending,
-);
+_UserSettings _$UserSettingsFromJson(Map<String, dynamic> json) =>
+    _UserSettings(
+      id: json['id'] as String,
+      defaultDrinkType: DrinkType.fromJson(
+        json['defaultDrinkType'] as Map<String, dynamic>,
+      ),
+      defaultDrinkSize: (json['defaultDrinkSize'] as num).toInt(),
+      drinkListSortOrder:
+          $enumDecodeNullable(
+            _$DrinkListSortOrderEnumMap,
+            json['drinkListSortOrder'],
+          ) ??
+          DrinkListSortOrder.descending,
+    );
 
-Map<String, dynamic> _$UserSettingsToJson(UserSettings instance) =>
+Map<String, dynamic> _$UserSettingsToJson(_UserSettings instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'defaultDrinkType': instance.defaultDrinkType.toJson(),
+      'defaultDrinkType': instance.defaultDrinkType,
       'defaultDrinkSize': instance.defaultDrinkSize,
-      'drinkListSort': _$DrinkListSortEnumMap[instance.drinkListSort]!,
+      'drinkListSortOrder':
+          _$DrinkListSortOrderEnumMap[instance.drinkListSortOrder]!,
     };
 
-const _$DrinkListSortEnumMap = {
-  DrinkListSort.descending: 'descending',
-  DrinkListSort.ascending: 'ascending',
+const _$DrinkListSortOrderEnumMap = {
+  DrinkListSortOrder.descending: 'descending',
+  DrinkListSortOrder.ascending: 'ascending',
 };
