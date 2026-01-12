3c3e3185d43c1516cc947774425526b3fd69135b
Ondřej Hložek <hlozek.o1@gmail.com>
2026-01-12 01:22:37 +0100

    refactor: move optimistic to timestamp converter

--- lib/drink/model/drink.dart
+++ lib/drink/model/drink.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:remembeer/common/converter/geopoint_converter.dart';
 import 'package:remembeer/common/converter/timestamp_converter.dart';
-import 'package:remembeer/common/converter/timestamp_converter_optimistic.dart';
 import 'package:remembeer/common/model/entity.dart';
 import 'package:remembeer/drink_type/model/drink_type.dart';
 
--- lib/drink_type/model/drink_type.dart
+++ lib/drink_type/model/drink_type.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:remembeer/common/converter/timestamp_converter.dart';
-import 'package:remembeer/common/converter/timestamp_converter_optimistic.dart';
 import 'package:remembeer/common/model/entity.dart';
 import 'package:remembeer/drink_type/model/drink_category.dart';
 
