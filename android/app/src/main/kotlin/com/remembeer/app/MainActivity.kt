05d088903e50a03f5c6591cbf1aa14c8689ade5e
Matej Klima <matej.klima5@gmail.com>
2026-09-12 18:02:12 +0200

    feat: change app id
    com.example would be rejected

--- android/app/src/main/kotlin/com/remembeer/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/remembeer/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.remembeer
+package com.remembeer.app
 
 import android.content.Intent
 import android.os.Bundle
--- android/app/src/main/kotlin/com/remembeer/app/QuickAddWidgetProvider.kt
+++ android/app/src/main/kotlin/com/remembeer/app/QuickAddWidgetProvider.kt
@@ -1,4 +1,4 @@
-package com.example.remembeer
+package com.remembeer.app
 
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
